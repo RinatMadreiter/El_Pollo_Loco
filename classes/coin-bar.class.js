fa762fe2a817c03e7681eba1486f76344f08ca84
Rinat Madreiter <umfragmail@gmx.de>
2022-03-09 00:03:07 +0100

    collect coin fixed

--- classes/coin-bar.class.js
+++ classes/coin-bar.class.js
@@ -53,13 +53,13 @@ class CoinBar extends DrawableObject {
     resolveImageIndex() {
         if (this.amountOfCoins == 0) {
             return 0;
-        } else if (this.amountOfCoins == 1 || 2 || 3) {
+        } else if (this.amountOfCoins == 1 || this.amountOfCoins == 2 || this.amountOfCoins == 3) {
             return 1;
         } else if (this.amountOfCoins == 4 || this.amountOfCoins == 5 ) {
             return 2;
-        } else if (this.amountOfCoins == 6 || 7) {
+        } else if (this.amountOfCoins == 6 || this.amountOfCoins == 7) {
             return 3;
-        } else if (this.amountOfCoins == 8 || 9) {
+        } else if (this.amountOfCoins == 8 || this.amountOfCoins == 9) {
             return 4;
         } else if (this.amountOfCoins == 10) {
             return 5;

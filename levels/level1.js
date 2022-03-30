4707f8ca5f23e99bca844f7db5da9abc955adb02
Rinat Madreiter <umfragmail@gmx.de>
2022-03-31 01:17:03 +0200

    added start screen and start button working

--- levels/level1.js
+++ levels/level1.js
@@ -1,84 +1,103 @@
-const level1 = new Level(
+let level1;
 
-    [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ],
-    [
-        new Endboss()
-    ],
-    [
-        new Cloud(),
-        new Cloud(),
-        new Cloud(),
-        new Cloud(),
-        new Cloud(),
-        new Cloud(),
-        new Cloud()
-    ],
-    [
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -720),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -720),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -720),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 720),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 720),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 720),
+function startGame() {
+    initLevel1();
+    init();
+    setTimeout(() => {
+        hideStartScreenDisplayGameContent();
+    }, 500);
+}
 
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 1440),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 1440),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 1440),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 2160),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 2160),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 2160),
+function initLevel1() {
 
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 2880),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 2880),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 2880),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 3600),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 3600),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 3600)
-    ],
-    [
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin(),
-        new Coin()
+    level1 = new Level(
 
-    ],
-    [
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle(),
-       new Bottle()
-    ]
-);
+        [
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Chicken()
+        ],
+        [
+            new Endboss()
+        ],
+        [
+            new Cloud(),
+            new Cloud(),
+            new Cloud(),
+            new Cloud(),
+            new Cloud(),
+            new Cloud(),
+            new Cloud()
+        ],
+        [
+            new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -720),
+            new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -720),
+            new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -720),
+            new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
+            new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
+            new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0),
+            new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 720),
+            new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 720),
+            new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 720),
+
+            new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 1440),
+            new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 1440),
+            new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 1440),
+            new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 2160),
+            new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 2160),
+            new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 2160),
+
+            new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 2880),
+            new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 2880),
+            new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 2880),
+            new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 3600),
+            new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 3600),
+            new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 3600)
+        ],
+        [
+            new Coin(),
+            new Coin(),
+            new Coin(),
+            new Coin(),
+            new Coin(),
+            new Coin(),
+            new Coin(),
+            new Coin(),
+            new Coin(),
+            new Coin()
+
+        ],
+        [
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle(),
+            new Bottle()
+        ]
+    );
 
 
- 
+}
+
+function hideStartScreenDisplayGameContent() {
+    document.getElementById('startScreen').classList.add('d-none');
+    document.getElementById('gameContent').classList.remove('d-none');
+}
+
+
